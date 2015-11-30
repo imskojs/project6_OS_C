91e927665b30febe4d878285c21bf62badb7f227
Seunghoon Ko <imskojs@gmail.com>
2015-11-30 19:40:38 +0900

    betat

--- codes/js/config/constant.js
+++ codes/js/config/constant.js
@@ -2,9 +2,9 @@
   'use strict';
 
   angular.module('app')
-  // .constant("governorUrl", "http://52.192.13.221")
-  .constant("governorUrl", "http://192.168.0.65:1337")
-    .constant("appName", "ohShop")
+    .constant("governorUrl", "http://52.192.13.221")
+  // .constant("governorUrl", "http://192.168.0.65:1337")
+  .constant("appName", "ohShop")
     .constant("appId", 7)
     .constant("googlePushSenderID", "350504486209")
     .constant("kakaoKey", "bdb254de02ea0b7521635ba469608674")
